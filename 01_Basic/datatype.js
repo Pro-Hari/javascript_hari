8b90a054ecc4aa28966b085f0e80df798df3e68b
Harsh Agarwal <152256877+Pro-Hari@users.noreply.github.com>
2023-11-29 09:46:02 +0000

    Conversion

--- 01_Basic/datatype.js
+++ 01_Basic/datatype.js
@@ -2,14 +2,15 @@
 
 //alert(5+6) we are using node.js not browser
 
-// console.log()
+// console.log("Harsh")  console.log(3+4) this is a bad practice of doing programming
+//code must be readable
 
 let name = "Harsh"
 let age = 23
 let isLoggedIn = true
 
 //number  => range(2 to power 53)
-//Bigint  => greater than the number range
+//Bigint  => greater than the number range // it is used in trading or stock market
 //string  => ""
 //boolean => true/false
 //null    => it is stand alone value(it is the representation of empty value)
@@ -18,6 +19,37 @@ let isLoggedIn = true
 
 //object 
 
-console.log(typeof null) // object
-console.log(typeof undefined) // undefined
-console.log(typeof isLoggedIn);
+// console.log(typeof null) // object
+// console.log(typeof undefined) // undefined
+// console.log(typeof isLoggedIn);
+
+
+// let score= "32"
+// console.log(typeof (score)) // we can also write in this way
+// console.log(typeof score)
+
+let a = null
+let c = Symbol
+let d = Number(c)
+
+// let score1 = Number(score)
+// console.log(typeof score1)
+
+// let b = Number(a)
+// console.log(b)
+// console.log(typeof b)
+// console.log(d)
+
+
+// let isLog = 1;
+// let g = Boolean(isLog)
+// console.log(g)
+
+// let is = ""
+// let f = Boolean(is)
+// console.log(f)
+
+
+let gh = 65
+let gf = String(65)
+console.log(typeof gf)
