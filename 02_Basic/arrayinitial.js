f09155b6d784a1face25a7f977d3485b954dfaa2
Harsh Agarwal <152256877+Pro-Hari@users.noreply.github.com>
2023-11-30 13:41:07 +0000

    array

---
+++ 02_Basic/arrayinitial.js
@@ -0,0 +1,37 @@
+const arr= [1,2,3,4,5,6]
+const heros = ["thor","spiderman"]
+
+console.log(arr)
+const myArr = new Array(1,2,3,4)
+
+console.log(myArr)
+
+// arr.push(8)
+// arr.push(9)
+// arr.pop()
+// console.log(arr)
+
+// arr.unshift(0) 
+// add element in the starting
+// console.log(arr);
+// arr.shift()
+
+// console.log(arr);
+
+// console.log(arr.includes(7)) // false
+// console.log(arr.indexOf(19)) // -1
+
+// const newArr = arr.join('$')// it binds into string
+
+// console.log(newArr)
+
+//Slice and Splice
+
+console.log("A" , arr)
+
+const mykk = arr.slice(1,3)
+console.log(mykk)
+
+const myth = arr.splice(3,3)
+console.log("B", arr)
+console.log(myth)
