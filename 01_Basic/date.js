4afbd2bf35321c4b65b503414b3dde74b8d397ff
Harsh Agarwal <152256877+Pro-Hari@users.noreply.github.com>
2023-11-30 13:16:53 +0000

    novend

---
+++ 01_Basic/date.js
@@ -0,0 +1,40 @@
+// Date
+// dates are always evaluated in milliseconds
+
+const d = new Date()
+// console.log(d)
+console.log(typeof d)
+// console.log(d.toString())
+
+// console.log(d.toDateString())
+
+// console.log(d.toLocaleString())
+
+// console.log(d.toLocaleDateString()) //11/30/23
+// console.log(d.toLocaleTimeString()) // 12:54:43 PM
+
+
+// let mycreateDate = new Date(2023,5,11,5,3)
+
+// let mycreateDate = new Date("2023-01-14")
+let mycreateDate = new Date("01-18-2023")
+// console.log(mycreateDate.toLocaleString())
+
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(mycreateDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000))
+
+console.log(d.getDate())
+console.log(d.getMonth()+1)
+
+
+
+
+
+
+
+
+
--- 01_Basic/number.js
+++ 01_Basic/number.js
@@ -7,35 +7,46 @@ const scsc = new Number(233)
 // console.log(scsc.toString().split(""))
 // console.log(scsc.toFixed(2))
 
-const other = 1213.8656
-console.log(other.toPrecision(3))
-console.log(other.toFixed(2))
+// const other = 1213.8656
+// console.log(other.toPrecision(3))
+// console.log(other.toFixed(2))
 
-const hun = 1000000
-console.log(hun.toLocaleString('en-IN'))
+// const hun = 1000000
+// console.log(hun.toLocaleString('en-IN'))
 
-const ffd = Number.MAX_VALUE
-console.log(ffd)
-
-
-//Mathematics
+// const ffd = Number.MAX_VALUE
+// console.log(ffd)
 
 
+// ************ Mathematics **************
+
+console.log(Math)
 const fd = Math.pow(2,4)
 console.log(fd)
 
-console.log(Math.abs(-5))
+// console.log(Math.abs(-5))
 
-console.log(Math.round(4.3))
+// console.log(Math.round(4.3))
 
-console.log(Math.ceil(4.9))
+// console.log(Math.ceil(4.9))
 
-console.log(Math.sqrt(25))
+// console.log(Math.sqrt(25))
 
-console.log(Math.max(2,7,5,9,1))
+// console.log(Math.max(2,7,5,9,1))
 
 
 console.log(Math.random()*10) // It always return the value between 0,1
+
+// console.log((Math.random()*10) +1)
+//If we want the value in a range using random method
+//like we want 10 to 20 random number
+
+const mn = 10
+const mx = 20
+
+console.log((Math.floor(Math.random() * (mx-mn+1)))+mn)
+
+
 
 
 
