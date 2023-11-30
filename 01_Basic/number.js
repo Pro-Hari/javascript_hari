3ad3aed3e6e979446e8144627538ee2f8f0ae955
Harsh Agarwal <152256877+Pro-Hari@users.noreply.github.com>
2023-11-30 03:55:18 +0000

    num

---
+++ 01_Basic/number.js
@@ -0,0 +1,41 @@
+const sc= 100
+// console.log(sc)
+
+const scsc = new Number(233)
+// console.log(scsc)
+
+// console.log(scsc.toString().split(""))
+// console.log(scsc.toFixed(2))
+
+const other = 1213.8656
+console.log(other.toPrecision(3))
+console.log(other.toFixed(2))
+
+const hun = 1000000
+console.log(hun.toLocaleString('en-IN'))
+
+const ffd = Number.MAX_VALUE
+console.log(ffd)
+
+
+//Mathematics
+
+
+const fd = Math.pow(2,4)
+console.log(fd)
+
+console.log(Math.abs(-5))
+
+console.log(Math.round(4.3))
+
+console.log(Math.ceil(4.9))
+
+console.log(Math.sqrt(25))
+
+console.log(Math.max(2,7,5,9,1))
+
+
+console.log(Math.random()*10) // It always return the value between 0,1
+
+
+
