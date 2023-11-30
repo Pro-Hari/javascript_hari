8c0705f781fb6b416d43dbd53f2775c6ce122a51
Harsh Agarwal <152256877+Pro-Hari@users.noreply.github.com>
2023-11-30 02:38:38 +0000

    sym

---
+++ 01_Basic/primitive.js
@@ -0,0 +1,42 @@
+// Primitive Dataype
+
+// 7 Types =>  Number, string, null, BigInt, Boolean, Undefined, Symbol
+
+const score = 34
+const scoreDec = 32.2
+
+const isLof = true
+const out = null
+let hel;
+
+const id = Symbol("asf")
+const id2  = Symbol("asf")
+
+const big = 3428749829874922298799n
+
+console.log(big)
+console.log(id == id2)
+
+
+// Non-Primitive DataTypes
+
+//Arrays, objects, Functions
+
+const arr = ["hello","harsh","shdhn"]
+
+let obj = {
+    name:"harsh",
+    age:43,
+    email:"harsh45@gmail.com"
+};
+const fun = function(){
+    console.log("hii")
+}
+console.log(arr)
+console.log(obj)
+console.log(fun());
+
+
+
+
+// just master web events and objects in javaScript
